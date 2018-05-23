a088b4d5fd1f7707dcb69ab496392f0e8054acf4
Kelly Gibbens <gibbenskd@gmail.com>
2018-05-22 21:21:33 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ObliqueStrategies/>
+  </div>
+</template>
+
+<script>
+import ObliqueStrategies from './components/ObliqueStrategies'
+
+export default {
+  name: 'App',
+  components: {
+    ObliqueStrategies
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ObliqueStrategies.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="oblique-strategies">
+    <h1>Oblique Strategies</h1>
+    <h2>By Brian Eno & Peter Schmidt</h2>
+    <div class="card">
+      <h3 class="card-text">{{card.strategy}}</h3>
+    </div>
+    <div class="btn">
+      <button @click.prevent="randomCard">New Card</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import obliqueStratgies from '../assets/obliqueStrategies.json'
+export default {
+  name: 'ObliqueStrategies',
+  data () {
+    return {
+      cards: obliqueStratgies,
+      card: ''
+    }
+  },
+  methods: {
+    randomCard () {
+      this.card = this.cards[Math.floor(Math.random() * this.cards.length)]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
