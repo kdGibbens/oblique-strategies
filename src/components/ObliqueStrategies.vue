adc13dfb3dee68d233fc0c6690e631dab527c8fc
Kelly Gibbens <gibbenskd@gmail.com>
2018-05-22 21:37:10 -0500

    styles for card.

--- src/components/ObliqueStrategies.vue
+++ src/components/ObliqueStrategies.vue
@@ -18,7 +18,7 @@ export default {
   data () {
     return {
       cards: obliqueStratgies,
-      card: ''
+      card:''
     }
   },
   methods: {
@@ -31,5 +31,41 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  body {
+    margin: 0;
+    padding: 0;
 
+  }
+  .card {
+    width: 350px;
+    height: 200px;
+    background: #f8f8ff;
+    box-shadow: 0 0 5px 0 rgba(50,50,50,.75);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    padding: 1rem;
+    border-radius: 1rem;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  button {
+    width: 10rem;
+    height: 2rem;
+    border: none;
+    background: #bb3a23;
+    border-radius: 1rem;
+    color: #f8f8ff;
+    font-size: 1rem;
+    font-weight: 700;
+    opacity: 0.9;
+  }
+  button:hover {
+    opacity: 1;
+  }
+  button:focus {
+    outline: none;
+  }
 </style>
